c8b5cca8a0ef58a9964f27a3bc2b40191ccba1f3
Jessie Cris Vicerra <jclvicerra@gmail.com>
2016-04-16 19:24:50 +0800

    refactor chikka api

--- routes/default/index.js
+++ routes/default/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var path = require('path');
+var ChikkaService = require(path.resolve("./models/services/ChikkaApiService"));
 // var passport = require('passport');
 // var models = require(path.resolve("./models/orm"));
 // var auth = require(path.resolve("./models/auth"));
@@ -16,20 +17,41 @@ router.get('/login', function(req, res, next) {
   res.renderLayout('public/login', { title: 'Express' }, '');
 });
 
+
 /* Chikka API Message Receiver */
+
+//curl --data "message_type=INCOMING&mobile_number=639173859454&shortcode=29290170414&timestamp=343453&request_id=77575&message=akoaymaysakit" http://localhost:3000/chikka/receive
 router.post('/chikka/receive', function(req, res, next) {
- res.send('POST request to the homepage');
+
+	if(req.body.message_type && req.body.message_type.toUpperCase() != "INCOMING"){
+		return  res.status(400).send('Not an Incoming message');
+	}
+  	
+	var messageData = {
+		message : req.body.message,
+		mobile_number: req.body.mobile_number,
+		shortcode : req.body.shortcode,
+		timestamp : req.body.timestamp,
+		request_id :req.body.request_id
+	};
+
+	console.log(messageData);
+  	//Process received data
+  	//Create ticket
+ 	res.status(200).send('POST request to the homepage');
 });
 
+//For testing purposes only
 router.get('/chikka/test', function(req, res, next) {
 
     var chikkaService = new ChikkaService();
 
-    chikkaService.send('639173859454', "Testing api", function(error, result){
+    /*chikkaService.send('639173859454', "Testing api", function(error, result){
         if (error) return res.send(400, error);
         
         res.send('POST request to the homepage');
-    });
+    });*/
+    res.status(200).send('POST request to the homepage');
 });
 
 module.exports = router;
