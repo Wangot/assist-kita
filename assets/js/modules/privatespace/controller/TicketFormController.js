bcac6d18fd602ba1b1d233c2daaaefa46f031b78
CJ <christian.balagbag@gmail.com>
2016-04-16 23:35:53 +0800

    intial reply

--- assets/js/modules/privatespace/controller/TicketFormController.js
+++ assets/js/modules/privatespace/controller/TicketFormController.js
@@ -7,9 +7,30 @@ var angular = require('angular');
 var PrivatespaceModuleBaseController = require('../base/PrivatespaceModuleBaseController');
 var CoreObjectUtilities = require('../../core/utilities/CoreObjectUtilities');
 
-function TicketController($routeParams) {
+function TicketController($routeParams, SimpleRestClientService, $location) {
   PrivatespaceModuleBaseController.call(this);
   var vm = this;
+
+  vm.ticketId = $routeParams.id;
+  vm.ticketContent = null;
+
+  vm.reply = function(toClose) {
+  	var ticketModel = SimpleRestClientService('tickets/'+ vm.ticketId +'/sub-tickets');
+
+  	var data = {
+  		content: vm.ticketContent
+  	};
+
+  	if (toClose) data.status = 'CLOSED';
+
+
+  	ticketModel.save(data)
+  	.then(function(res) {
+  		 $location('/websites');
+  	});
+
+  }
+
 }
 
 CoreObjectUtilities.inherit(TicketController, PrivatespaceModuleBaseController);
