9a9a3f8c41f365fe9da8c6716ce3889394ad3daa
Jessie Cris Vicerra <jclvicerra@gmail.com>
2016-04-16 16:56:59 +0800

    initial chikka

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var ChikkaService = require('../services/ChikkaApiService.js');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
@@ -8,6 +9,22 @@ router.get('/', function(req, res, next) {
 /* GET home page. */
 router.get('/privatespace', function(req, res, next) {
   res.renderLayout('index', { title: 'Express' }, 'privatespace/index');
+});
+
+/* Chikka API Message Receiver */
+router.post('/chikka/receive', function(req, res, next) {
+ res.send('POST request to the homepage');
+});
+
+router.get('/chikka/test', function(req, res, next) {
+
+	var chikkaService = new ChikkaService();
+
+	chikkaService.send('639173859454', "Testing api", function(error, result){
+		if (error) return res.send(400, error);
+        
+        res.send('POST request to the homepage');
+	});
 });
 
 module.exports = router;
---
+++ services/ChikkaApiService.js
@@ -0,0 +1,65 @@
+var q = require('q');
+var querystring = require('querystring');
+var http = require('http');
+
+function ChikkaApiService() {
+
+	var CHIKKA_API_URL = 'post.chikka.com'
+	var API_SEND_REQUEST_URL = '/smsapi/request';
+	var API_SECRET = '60dc5cdd9621c51942f8637da7d41b4bf331779b114047f95df7f052f67fd9da';
+	var API_CLIENT_ID = 'c6d706ab5729596f18ed600962cd06004288c03f3c46874a7523528d39477311';
+	var SHORT_CODE = '29290170414';
+
+	this.send = function(mobileNumber, message, callback){
+		
+		if(mobileNumber.length <= 0 ){
+			console.log("Invalid mobile number");
+			return callback("Invalid mobile number");
+		}
+
+		var postData = querystring.stringify({
+	  		"message_type" : "SEND",
+	        "mobile_number" : mobileNumber, //"639181234567",
+	        "shortcode" : SHORT_CODE,
+	        "message_id" : "12345678901234567890123456789012",
+	        "message" : message,
+	        "client_id" : API_CLIENT_ID,
+	        "secret_key" : API_SECRET
+		});
+
+		var options = {
+			hostname: CHIKKA_API_URL,
+			port: 80,
+			path: API_SEND_REQUEST_URL,
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Content-Length': postData.length
+			}
+		};
+
+		var req = http.request(options, function(res) {
+			console.log('STATUS: ' + res.statusCode);
+			console.log('HEADERS: ' + JSON.stringify(res.headers));
+			res.setEncoding('utf8');
+			res.on('data', function (chunk) {
+				console.log('BODY: ' + chunk);
+				//callback(null, chunk);
+			});
+			res.on('end', function () {
+				callback(null, "success");
+			});
+		});
+
+		req.on('error', function (error) {
+				console.log(`problem with request: ${error}`);
+				callback(error);
+		});
+
+		// write data to request body
+		req.write(postData);
+		req.end();
+	}
+}
+
+module.exports = ChikkaApiService
