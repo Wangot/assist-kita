46303100ba1722d59408fa4837939eae73dd1727
CJ <christian.balagbag@gmail.com>
2016-04-17 10:12:15 +0800

    add pagination

--- assets/js/modules/privatespace/controller/HealthCenterListController.js
+++ assets/js/modules/privatespace/controller/HealthCenterListController.js
@@ -7,7 +7,7 @@ var angular = require('angular');
 var PrivatespaceModuleBaseController = require('../base/PrivatespaceModuleBaseController');
 var CoreObjectUtilities = require('../../core/utilities/CoreObjectUtilities');
 
-function HealthCenterListController($routeParams, $uibModal, SimpleRestClientService) {
+function HealthCenterListController($routeParams, $uibModal, SimpleRestClientService, $location) {
   PrivatespaceModuleBaseController.call(this);
   var vm = this;
 
@@ -16,13 +16,31 @@ function HealthCenterListController($routeParams, $uibModal, SimpleRestClientSer
   function _init() {
   	var HealthCenterModel = SimpleRestClientService('health-centers');
 
-  	HealthCenterModel.get()
+  	HealthCenterModel.get({page: $routeParams.page})
   	.then(function(res) {
   		vm.HealthCenters = res.HealthCenters;
+
+      vm.showList = true;
+
+      vm.page = res.params.page;
+      var perPage = res.params.perPage;
+      vm.perPage = perPage;
+
+      var totalItems = res.params.total;
+      vm.totalItems = totalItems;
+
+      var totalPages = Math.ceil(totalItems / perPage);
+      vm.totalPages = totalPages;
   	});
-
-
   }
+
+  this.go = function() {
+    $location.search(
+        {
+            'page': vm.page,
+        }
+    );
+  }  
 
   _init();
 
